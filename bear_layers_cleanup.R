27c7eb77eafb02088dfd49295dc704ac895f3d3c
popovs <sarah.popov@gmail.com>
2024-08-27 11:09:17 -0700

    Move token to separate file; bundle clean up into clean_bears() fxn

--- bear_layers_cleanup.R
+++ bear_layers_cleanup.R
@@ -3,10 +3,13 @@
 # Set token
 # Token seems to expire anytime your ArcGIS Online session expires -
 # seems to be roughly ~30 mins.
-token <- "3NKHt6i2urmWtqOuugvr9fdAOlJ-AnbloT9i0687pzIv10OGYc_T4PR4piOkKNW7XSPsN_c8z0JcKpx1mneoLDifHIglSoLk48s5jDMCfkEFN_hZDR_dJOZ6g0cOW3K07R1UQ4xFDLWjYEyXUDgWnMsGuzO1OFjA8lFiQF-Dwg31RxL66sHUpEOmunQrMfV_jPEc6WMPR1t_Ns_42tqPW9wcFb5N_Ea053xlsFJ6dOA."
+# Even though it expires super quickly, better practice to isolate token
+# value in some file that's not tracked by git
+source("temp/token.R")
 
 # Load functions
 source("R/fetch_agol_data.R")
+source("R/clean_bear_data.R")
 
 # Fetch bear data
 
@@ -18,14 +21,26 @@ dens <- fetch_bears(token = token,
 f <- fetch_bears(token = token,
                  layer = "field visits")
 
+# `p` for `potential` data
+p <- fetch_bears(token = token,
+                 layer = "potential")
+
+# Data backups
 # Store to `temp` folder as a data backup
+
 systime <- format(Sys.time(), "%Y%m%d-%H%M%S")
-write.csv(dens, paste0("temp/dens_current_", systime, ".csv"),
-          row.names = FALSE,
-          na = "")
+
+# Current
+sf::st_write(dens, paste0("temp/dens_current_", systime, ".shp"))
+
+# Field visits
 write.csv(f, paste0("temp/dens_visits_", systime, ".csv"),
           row.names = FALSE,
           na = "")
+
+# Potential
+sf::st_write(p, paste0("temp/dens_potential_", systime, ".shp"))
+
 rm(systime)
 
 # Keep original column names stored
@@ -33,26 +48,34 @@ rm(systime)
 # bridge when we get to it
 dens_cols <- names(dens)
 f_cols <- names(f)
+p_cols <- names(p)
 
-# Clean up column names
-dens <- janitor::clean_names(dens)
-names(dens) <- gsub("i_d", "id", names(dens)) # fix janitor splitting 'IDs' into 'i_ds'
-f <- janitor::clean_names(f)
-
-# Drop geometry column for visits table
-f <- sf::st_drop_geometry(f)
+#### GENERAL CLEANUP ####
+dens <- clean_bears(dens)
+f <- clean_bears(f)
+p <- clean_bears(p)
 
 
-#### CLEAN UP ####
+#### CHECK NULL OR DUPE IDs ####
 
 # Check for any NULL den ids
 any(is.na(dens$den_id)) # y u c k 
 any(is.na(f$den_id))
 
+# Check dud dens
+dens[is.na(dens$den_id),]
+dens[grep("test|delete", dens$den_id, ignore.case = T),]
+
+f[grep("test|delete", f$den_id, ignore.case = T),]
+
 # Check den ids is unique in dens
-dens$den_id %in% dens[["den_id"]][duplicated(dens$den_id)]
+dens$den_id %in% dens[["den_id"]][duplicated(dens$den_id)] # y u c k
 dens[dens$den_id %in% dens[["den_id"]][duplicated(dens$den_id)],]
 
 # Check sample_id is unique in visits
 f$sample_id %in% f[["sample_id"]][duplicated(f$sample_id)]
+f[(f$sample_id %in% f[["sample_id"]][duplicated(f$sample_id)]),] |>
+  dplyr::arrange(sample_id) |>
+  View()
+
 
